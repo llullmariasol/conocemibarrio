8d210230aa86df9cdfeb9034b167e79ebe309cbe
Sol Llull <llullmariasol@gmail.com>
2022-03-02 22:13:10 -0300

    Add MySQL database configuration, add .gitignore

--- conocemibarrio/settings.py
+++ conocemibarrio/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'conocemibarrio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'conocemibarrio',
+        'USER': 'root',
+        'PASSWORD': 'admin1234',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
