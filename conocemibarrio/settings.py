c227d4baa69ee1e574166c44d2c340309cab6faa
solllull <llullmariasol@gmail.com>
2022-05-09 18:48:51 -0300

    Update settings.py

--- conocemibarrio/settings.py
+++ conocemibarrio/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.gis',
     'django.contrib.staticfiles',
     'social_django',
     'allauth',
