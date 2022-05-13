dd75ced0238d5a3b1df5e119cd068d2e4d07fdf1
solllull <llullmariasol@gmail.com>
2022-05-13 20:48:35 -0300

    Update settings.py

--- conocemibarrio/settings.py
+++ conocemibarrio/settings.py
@@ -106,10 +106,8 @@ WSGI_APPLICATION = 'conocemibarrio.wsgi.application'
 #}
 
 DATABASES = {}
-#DATABASES['default'] = dj_database_url.config()
-DATABASES['default'] = dj_database_url.config()
-# Password validation
-# https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES.update(default=db_from_env)
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -194,6 +192,6 @@ GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
 # This should already be in your settings.py
 django_heroku.settings(locals())
 # This is new
-#options = DATABASES['default'].get('OPTIONS', {})
-#options.pop('sslmode', None)
+options = DATABASES['default'].get('OPTIONS', {})
+options.pop('sslmode', None)
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.mysql'
