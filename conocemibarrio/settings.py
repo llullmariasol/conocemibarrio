50f98f349e1f0b8b33ccde68691c8af13d8a0cb2
solllull <llullmariasol@gmail.com>
2022-08-07 20:14:38 -0300

    update settings

--- conocemibarrio/settings.py
+++ conocemibarrio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+gj0caznve=v!w387ens4gnv2qbz5fjbgl%k6efcvb(j+k0$7z
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['https://conocemibarrio.herokuapp.com']
+CSRF_TRUSTED_ORIGINS = ['https://*']
 CSRF_COOKIE_DOMAIN = '.herokuapp.com'
 
 # Application definition
