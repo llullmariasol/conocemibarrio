4870ae0bd1e84ec1391da1232696b742bd4d88d6
solllull <llullmariasol@gmail.com>
2022-08-09 18:27:53 -0300

    Update views.py

--- registration/views.py
+++ registration/views.py
@@ -209,7 +209,8 @@ def logIn(request):
 
 def logOut(request):
     logout(request)
-    # return HttpResponseRedirect('/')
+    return HttpResponseRedirect('/')
+
     return render(request, 'base.html', {})
 
 
