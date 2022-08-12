c7a80d81d10ca26f6e9f1bc38f54cccfc849bdce
solllull <llullmariasol@gmail.com>
2022-08-11 23:10:17 -0300

    Add map, show points of interest

--- realtime_map/views.py
+++ realtime_map/views.py
@@ -1,5 +1,31 @@
+import json
+
+from django.contrib.gis.geos import GEOSGeometry
 from django.shortcuts import render
+
+from neighborhood.models import (PointOfInterest, PointOfInterestDTO)
 
 
 def showRealTimeMap(request):
-    return render(request, 'realtime_map.html')
+    args = {}
+    points_of_interest = list(PointOfInterest.objects.all())
+    points = []
+
+    for point in points_of_interest:
+        point_dto = PointOfInterestDTO()
+        point_dto.id = point.pk
+        point_dto.name = point.name
+        point_dto.description = point.description
+        point_dto.location = getCoordsArray(point.location)
+        points.append(point_dto)
+
+    args['points'] = points
+
+    return render(request, 'realtime_map.html', args)
+
+
+def getCoordsArray(location):
+    geo_location = GEOSGeometry(location)
+    input_string = geo_location.geojson
+    coordinates_data = json.loads(input_string)
+    return coordinates_data['coordinates']
