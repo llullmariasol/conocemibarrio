79bcc6cdd6d5b1bbfd493373fda60dc7fea699db
solllull <llullmariasol@gmail.com>
2022-08-26 11:39:53 -0300

    update models.py

--- chat/models.py
+++ chat/models.py
@@ -2,10 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from registration.models import Neighborhood
 
+
 class NeighborhoodChat(models.Model):
     neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.neighborhood.name + ' chat'
+
 
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
